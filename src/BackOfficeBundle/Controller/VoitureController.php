82fc53b2e2d02a0aa66a3b18607e9898d1531467
Virkine <virgile.prin@gmail.com>
2018-04-20 17:59:13 +0200

    ignore me

---
+++ src/BackOfficeBundle/Controller/VoitureController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BackOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class VoitureController extends Controller
+{
+}
