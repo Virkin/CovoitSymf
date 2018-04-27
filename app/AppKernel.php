f2ac6e0f051c5bfeb1aa3d61d471580381b057c7
Antoine-Auffret <a.y.auffret@gmail.com>
2018-04-27 14:11:18 +0200

    add FrontOfficeBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new BackOfficeBundle\BackOfficeBundle(),
+            new FrontOfficeBundle\FrontOfficeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
