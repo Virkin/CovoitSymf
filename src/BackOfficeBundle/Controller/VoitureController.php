cbb5fe6d00f12046e1dd1dfc5eea057a399d58ce
Virkine <virgile.prin@gmail.com>
2018-04-30 19:53:31 +0200

    add marque works ( in addvoiture form ), but awful design

--- src/BackOfficeBundle/Controller/VoitureController.php
+++ src/BackOfficeBundle/Controller/VoitureController.php
@@ -5,7 +5,9 @@ namespace BackOfficeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use BackOfficeBundle\Entity\Voiture;
+use BackOfficeBundle\Entity\Marque;
 use BackOfficeBundle\Form\VoitureType;
+use BackOfficeBundle\Form\MarqueType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
@@ -43,6 +45,29 @@ class VoitureController extends Controller
 	    }
 
         return $this->render('BackOfficeBundle:Voiture:form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+	}
+
+	function addMarqueAction(Request $request)
+	{
+		$marque = new Marque();
+
+        $form = $this->createForm(MarqueType::class, $marque);
+
+        $form->handleRequest($request);
+      
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+	      	$em = $this->getDoctrine()->getManager();
+	        $em->persist($marque);
+	        $em->flush($marque);
+	        $voiture = $form->getData();
+	    	
+	        return $this->redirectToRoute('addVoiture');
+	    }
+
+        return $this->render('BackOfficeBundle:Voiture:marque.html.twig', array(
             'form' => $form->createView(),
         ));
 	}
---
+++ src/BackOfficeBundle/Form/MarqueType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BackOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+
+class MarqueType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('cancel', ButtonType::class, array('label' => 'Cancel'))
+            ->add('save', SubmitType::class, array('label' => 'Save'));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackOfficeBundle\Entity\Marque'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backofficebundle_marque';
+    }
+
+
+}
