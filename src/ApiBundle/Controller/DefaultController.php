767dad85f40262f053a37fb030defb5032c0ac4f
Antoine-Auffret <antoine@auffret.ovh>
2018-05-26 00:46:59 +0200

    remove findTrajetsRest and use same functions from the front office for the api

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -7,11 +7,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use BackOfficeBundle\Repository\TrajetRepository;
-use BackOfficeBundle\Entity\Internaute;
-use BackOfficeBundle\Entity\Marque;
-use BackOfficeBundle\Entity\Trajet;
-use BackOfficeBundle\Entity\Ville;
-use BackOfficeBundle\Entity\Voiture;
 
 class DefaultController extends Controller
 {
@@ -22,9 +17,37 @@ class DefaultController extends Controller
 
     public function listTrajetsAction(Request $request)
     {
-      $trajets = $this->getDoctrine()
-      ->getRepository('BackOfficeBundle:Trajet')
-      ->findTrajetsRest();
+      // recupere les arguments passes par url
+      $filter = $request->query->get('filter');
+      $search = $request->query->get('search');
+      $id = $request->query->get('id');
+
+      // si id existe, retourne le trajet
+      if ($id != "" or $id != null) {
+        $trajets = $this->getDoctrine()
+        ->getRepository('BackOfficeBundle:Trajet')
+        ->findById($id);
+      }
+      else {
+        // si pas de filter ou deparr, retourne tous les trajets
+        if ($filter == null or $filter == "" or $filter == "deparr" ) {
+          $trajets = $this->getDoctrine()
+          ->getRepository('BackOfficeBundle:Trajet')
+          ->findAllOrderedByName($search);
+        }
+        // si filtre dep, retourne tous les trajets dep
+        elseif ($filter == "dep") {
+          $trajets = $this->getDoctrine()
+          ->getRepository('BackOfficeBundle:Trajet')
+          ->findDepOrderedByName($search);
+        }
+        // si filtre arr, retourne tous les trajets arr
+        elseif ($filter == "arr") {
+          $trajets = $this->getDoctrine()
+          ->getRepository('BackOfficeBundle:Trajet')
+          ->findArrOrderedByName($search);
+        }
+      }
 
       return new JsonResponse([
             $trajets
