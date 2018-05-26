c76fe5a900d60439995dc6ed72ba52fc1f83abda
Antoine-Auffret <antoine@auffret.ovh>
2018-05-27 00:22:01 +0200

    add edit and delete internaute pages

--- src/BackOfficeBundle/Controller/InternauteController.php
+++ src/BackOfficeBundle/Controller/InternauteController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 use BackOfficeBundle\Entity\Internaute;
 use BackOfficeBundle\Repository\VoitureRepository;
 use BackOfficeBundle\Form\InternauteType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
 class InternauteController extends Controller
 {
@@ -45,6 +47,60 @@ class InternauteController extends Controller
         return $this->render('BackOfficeBundle:Internaute:form.html.twig', array(
             'form' => $form->createView(),
             'voitures' => $voitures
+        ));
+	}
+
+	function editAction(Request $request, $id)
+	{
+		$internaute = $this->getDoctrine()
+        ->getRepository('BackOfficeBundle:Internaute')
+        ->find($id);
+
+    	$form = $this->createForm(InternauteType::class, $internaute);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+	      	$em = $this->getDoctrine()->getManager();
+	        $em->persist($internaute);
+	        $em->flush($internaute);
+	        $internaute = $form->getData();
+
+	        return $this->redirectToRoute('readInternaute');
+	    }
+
+			$voitures = $this->getDoctrine()->getRepository('BackOfficeBundle:Voiture')->findVoitureWithMarque();
+
+        return $this->render('BackOfficeBundle:Internaute:form.html.twig', array(
+            'form' => $form->createView(),
+						'voitures' => $voitures
+        ));
+	}
+
+	function deleteAction(Request $request, $id)
+	{
+		$form = $this->createFormBuilder()
+            ->add('cancel', ButtonType::class, array('label' => 'Cancel'))
+            ->add('delete', SubmitType::class, array('label' => 'Delete'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid())
+        {
+			$em = $this->getDoctrine()->getManager();
+	        $internaute = $this->getDoctrine()
+	        ->getRepository('BackOfficeBundle:Internaute')
+	        ->find($id);
+	        $em->remove($internaute);
+	        $em->flush($internaute);
+
+			return $this->redirectToRoute('readInternaute');
+
+		}
+		return $this->render('BackOfficeBundle:Internaute:delete.html.twig', array(
+            'form' => $form->createView(),
         ));
 	}
 }
