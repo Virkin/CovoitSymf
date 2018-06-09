1d4153e0090d8704fe4b2e16537ed09754b607f2
Virkine <virgile.prin@gmail.com>
2018-06-09 15:19:59 +0200

    stats data /TODO : display data

--- src/BackOfficeBundle/Controller/StatistiquesController.php
+++ src/BackOfficeBundle/Controller/StatistiquesController.php
@@ -15,10 +15,27 @@ class StatistiquesController extends Controller
 	    ->findAll();
 
 		$em = $this->container->get('doctrine')->getManager();
-		$qb = $em->createQueryBuilder();
-		$qb->select('count(Internaute.id)');
-		$qb->from('BackOfficeBundle:Internaute','Internaute');
-		$nb_internautes = $qb->getQuery()->getSingleScalarResult();
+		
+		$qb = $em->createQuery("SELECT count(Internaute.id) FROM BackOfficeBundle:Internaute as Internaute");
+		
+		$nb_internautes = $qb->getSingleScalarResult();
+
+		$qb = $em->createQuery("SELECT count(trajet.id) AS nb_trajet
+		FROM BackOfficeBundle:Trajet as trajet
+		GROUP BY trajet.internaute
+		HAVING count(trajet.id) >= 2");
+
+		$nb_big_internautes = count($qb->getResult());
+
+		$qb = $em->createQuery("SELECT ville as name, count(tr.id) AS HIDDEN nbTrajet FROM BackOfficeBundle:trajet AS tr JOIN BackOfficeBundle:ville AS ville WITH ville.id=tr.villeArr GROUP BY tr.villeArr ORDER BY nbTrajet DESC");
+		
+		$topVilleArr = $qb->getResult();
+		dump($topVilleArr);
+
+		$qb = $em->createQuery("SELECT ville as name, count(tr.id) AS HIDDEN nbTrajet FROM BackOfficeBundle:trajet AS tr JOIN BackOfficeBundle:ville AS ville WITH ville.id=tr.villeDep GROUP BY tr.villeDep ORDER BY nbTrajet DESC");
+		
+		$topVilleDep = $qb->getResult();
+		dump($topVilleDep);
 
 	    if (!$statistiques) {
 	        $nb_internautes = NULL;
@@ -26,6 +43,9 @@ class StatistiquesController extends Controller
 
 	    return $this->render('BackOfficeBundle:Statistiques:read.html.twig', array(
 	        'nb_internautes' => $nb_internautes,
+	        'nb_big_internautes' => $nb_big_internautes,
+	        'topVilleArr' => $topVilleArr,
+	        'topVilleDep' => $topVilleDep
 	    ));
 	}
 }
